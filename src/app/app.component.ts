cf2353f56bbdfbb57bb4d5f61131bab0c8e0f714
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-11 23:30:57 +0200

    Shadow adjustment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Single tree',
+      username: '@Nature',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus mollitia eum pariatur adipisci obcaecati maxime culpa amet minima quis fuga!',
+      imageUrl: 'assets/tree.png',
+    },
+    {
+      title: 'Old school transport',
+      username: '@Active',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus mollitia eum pariatur adipisci obcaecati maxime culpa amet minima quis fuga!',
+      imageUrl: 'assets/bike.png',
+    },
+    {
+      title: 'Good to have friends',
+      username: '@Social',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus mollitia eum pariatur adipisci obcaecati maxime culpa amet minima quis fuga!',
+      imageUrl: 'assets/socials.jpg',
+    },
+  ];
 }
